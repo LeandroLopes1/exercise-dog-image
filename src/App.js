1977164574db9fac2a5bce044e91760cab02b066
Leandro Lopes <leandro_carlos@uol.com.br>
2021-06-07 12:07:37 -0300

    exercicio bloco 13.1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './components/Profile';
+import Connections from './components/Connections';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showProfile: true,
+    };
+
+    this.changeProfile = this.changeProfile.bind(this);
+  }
+
+  changeProfile() {
+    const { showProfile } = this.state;
+
+    this.setState({ showProfile: !showProfile });
+  }
+
+  render() {
+    const { showProfile } = this.state;
+
+    return (
+      <div className="gitNetwork d-flex flex-column justify-content-center">
+        { showProfile ? <Profile /> : null }
+        <div className="central d-flex justify-content-center">
+          <button
+            className="btn btn-dark align-self-center"
+            type="button"
+            onClick={ this.changeProfile }
+          >
+            Mostrar / Ocultar Perfil
+          </button>
+        </div>
+        <Connections />
+      </div>
+    );
+  }
 }
 
 export default App;
